e9b79302e0806d98987e295c1f165df1d3f75648
Pelltire <73190861+Pelltire@users.noreply.github.com>
2020-10-27 14:28:50 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-	
+    if (_switch == 0) {
+        nombre_1 += 1
+        basic.showNumber(nombre_1)
+    }
+    if (_switch == 1) {
+        nombre_2 += 1
+        basic.showNumber(nombre_2)
+    }
 })
-let nombre_1 = 1
-basic.showNumber(nombre_1)
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    _switch += 1
+    if (_switch == 2) {
+        basic.showNumber(nombre_1 + nombre_2)
+    }
+})
+let _switch = 0
 let nombre_2 = 0
+let nombre_1 = 0
+nombre_1 = 0
+nombre_2 = 0
+_switch = 0
